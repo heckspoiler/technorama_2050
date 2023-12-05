70e8a74e4d705b36898f40e915d6cc30994a7b41
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-12-05 17:12:51 +0100

    wordpress connected, api calls work, ui updates

--- utils/api.js
+++ utils/api.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 export async function fetchWordpressData() {
   const response = await axios.get(
-    'http://technorama2050.local/wp-json/wp/v2/posts'
+    process.env.NEXT_PUBLIC_WORDPRESS_REST_ENDPOINT
   );
   console.log(response.data);
   return response.data;
 }
+
+fetchWordpressData();
