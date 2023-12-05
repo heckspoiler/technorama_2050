3c9c7bcc6665f58818470663913959336e5a86f2
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-12-05 13:13:02 +0100

    connecting headless cms to next.js

---
+++ app/layout.js
@@ -0,0 +1,18 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Technorama 2050',
+  description:
+    'Technorama 2050 is an ongoing project by the Technorama museum in Winterthur, Switzerland. It is a collection of speculative design projects that explore the future of technology and society.',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
