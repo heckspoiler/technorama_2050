c9e724a1ce565bc1a4a116d79c6345965f3d1336
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-12-05 15:48:47 +0100

    connected to wordpress, data fetching doesn't work

--- utils/api.js
+++ utils/api.js
@@ -6,25 +6,33 @@ export async function fetchWordpressData() {
     cache: new InMemoryCache(),
   });
 
-  const data = await client.query({
-    query: gql`
-      query getPost($id: ID = "", $authorName: String = "") {
-        pages(first: 10) {
-          nodes {
-            authorId
+  try {
+    const { data } = await client.query({
+      query: gql`
+        query getPost($id: ID = "", $authorName: String = "") {
+          pages(first: 10) {
+            nodes {
+              authorId
+            }
           }
-        }
-        post(asPreview: true) {
-          author {
-            node {
-              username
+          post(asPreview: true) {
+            author {
+              node {
+                username
+              }
             }
           }
         }
-      }
-    `,
-  });
+      `,
+    });
 
-  console.log(data);
-  return data.pages.nodes.authorId;
+    console.log(data);
+    // Assuming you want to return the authorIds of the pages
+    return data.pages.nodes.map((node) => node.authorId);
+  } catch (error) {
+    console.error('Error fetching WordPress data:', error);
+    // Handle the error based on your application's needs
+    // For example, you might want to return null or an empty array
+    return null;
+  }
 }
