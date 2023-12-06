276d8a3d148c7a5c8c02e46866223a6c698a7490
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-12-06 15:13:21 +0100

    just figuring stuff out, backend working again

--- app/home/page.js
+++ app/home/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 import { usePostsStore } from '../../stores/postStore';
-import { styles } from './home.module.css';
+import styles from './home.module.css';
 
 export default function Home() {
   const { wordPressPosts, setWordPressPosts, fetchWordPressPosts } =
@@ -12,15 +12,13 @@ export default function Home() {
     fetchWordPressPosts();
   }, [fetchWordPressPosts]);
 
-  console.log('Posts from Home: ', wordPressPosts);
-
   if (!wordPressPosts || wordPressPosts.length === 0) {
     return <h1>Loading posts...</h1>;
   }
 
   return (
     <>
-      <h1>Posts</h1>
+      <h1 className={styles.mainTitle}>Posts</h1>
       {wordPressPosts.map((post) => (
         <div key={post.id}>
           <h2>{post.title.rendered}</h2>
--- utils/api.js
+++ utils/api.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
 
 export async function fetchWordpressData() {
-  const response = await axios.get(
-    process.env.NEXT_PUBLIC_WORDPRESS_REST_ENDPOINT
-  );
-  console.log(response.data);
-  return response.data;
-}
+  try {
+    const response = await axios.get(
+      process.env.NEXT_PUBLIC_WORDPRESS_REST_ENDPOINT,
+      { headers: { 'Content-Type': 'application/json' } }
+    );
 
-fetchWordpressData();
+    if (response.status === 200) {
+      console.log(response.data);
+      return response.data;
+    } else {
+      console.error('Failed to fetch data: ', response.status);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching data: ', error);
+    return null;
+  }
+}
