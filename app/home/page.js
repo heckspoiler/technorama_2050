0e4a7d88372ae2ea8318b2f8160ed5caf5b035a0
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-12-07 14:00:27 +0100

    changes

--- app/home/page.js
+++ app/home/page.js
@@ -24,8 +24,30 @@ export default function Home() {
           <h2>{post.title.rendered}</h2>
           <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
           <div>{post.id}</div>
+          <div>{post.categories}</div>
         </div>
       ))}
+
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 703 184"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path id={styles.svgShape} fill="blue" stroke="black" />
+        <text
+          x="50%"
+          y="50%"
+          text-anchor="middle"
+          fill="white"
+          font-family="Verdana"
+          font-size="24"
+          dy=".3em"
+        >
+          Hover Me
+        </text>
+      </svg>
     </>
   );
 }
