60dd5531642f4b1f90f2b5b466d22a848c236f0d
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-12-07 16:28:37 +0100

    background effect added

--- components/home/secondSection/secondSection.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './secondSection.module.css';
-
-export const SecondSection = () => {
-  return <div className={styles.secondContainer}>SecondSection</div>;
-};
