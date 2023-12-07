bb507dc73f3cb0f877ed8081facb113984cd26d4
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-12-07 14:26:49 +0100

    adding different sections to home page

--- app/home/page.js
+++ app/home/page.js
@@ -2,6 +2,9 @@
 
 import { useEffect } from 'react';
 import { usePostsStore } from '../../stores/postStore';
+import { FirstSection } from '@/components/home/firstSection/FirstSection';
+import { SecondSection } from '@/components/home/secondSection/SecondSection';
+import { ThirdSection } from '@/components/home/thirdSection/thirdSection';
 import styles from './home.module.css';
 
 export default function Home() {
@@ -17,7 +20,7 @@ export default function Home() {
   }
 
   return (
-    <>
+    <div className={styles.mainContainer}>
       <h1 className={styles.mainTitle}>Posts</h1>
       {wordPressPosts.map((post) => (
         <div key={post.id}>
@@ -28,26 +31,9 @@ export default function Home() {
         </div>
       ))}
 
-      <svg
-        width="100%"
-        height="100%"
-        viewBox="0 0 703 184"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path id={styles.svgShape} fill="blue" stroke="black" />
-        <text
-          x="50%"
-          y="50%"
-          text-anchor="middle"
-          fill="white"
-          font-family="Verdana"
-          font-size="24"
-          dy=".3em"
-        >
-          Hover Me
-        </text>
-      </svg>
-    </>
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+    </div>
   );
 }
---
+++ components/home/firstSection/firstSection.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './firstSection.module.css';
+
+export const FirstSection = () => {
+  return <div>FirstSection</div>;
+};
