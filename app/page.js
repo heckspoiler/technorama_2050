62df9db9a1d046b7ac4abba0eb4146c014226aa8
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-12-11 15:59:26 +0100

    chages

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Home from './home/page';
+import { SvgBackground } from '@/components/home/svgBackground/svgBackground';
+import CustomCursor from '@/components/general/cursor/cursor';
+
+export default function App({ posts }) {
+  return (
+    <>
+      <CustomCursor />
+      <SvgBackground />
+      <Home />
+    </>
+  );
+}
---
+++ components/header/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link';
+import { DonateButton } from './donateButton/donateButton';
+import Logo from './logo/logo';
+import styles from './header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Link href="www.google.ch">
+        <Logo />
+      </Link>
+      <DonateButton />
+    </header>
+  );
+};
+
+export default Header;
