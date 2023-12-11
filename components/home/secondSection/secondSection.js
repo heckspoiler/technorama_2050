9b14c8d2ca83f4234456d3e49522f2cc64464e2f
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-12-11 14:22:23 +0100

    trying to add scroll stop for second and third section

--- components/home/secondSection/secondSection.js
+++ components/home/secondSection/secondSection.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './secondSection.module.css';
 
 export const SecondSection = () => {
-  return <div className={styles.secondContainer}>SecondSection</div>;
+  const [isSticky, setSticky] = useState(false); // Renamed from isFixed to isSticky
+
+  const checkSticky = () => {
+    const headerHeight = 6 * 16; // 6rem in pixels
+    const secondSection = document.querySelector(`.${styles.secondContainer}`);
+
+    if (secondSection) {
+      const rect = secondSection.getBoundingClientRect();
+      setSticky(rect.top <= headerHeight);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkSticky);
+
+    return () => {
+      window.removeEventListener('scroll', checkSticky);
+    };
+  }, [checkSticky]);
+
+  return (
+    <div
+      className={`${styles.secondContainer} ${isSticky ? styles.sticky : ''}`}
+    >
+      SecondSection
+    </div>
+  );
 };
