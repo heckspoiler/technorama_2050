f3ecfd47ab57f013fab5d6a3cd9c93cb98cd8b8e
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-12-11 17:22:08 +0100

    saving because of laptop acting werid

--- components/home/firstSection/firstSection.js
+++ components/home/firstSection/firstSection.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from './firstSection.module.css';
 
 export const FirstSection = () => {
   return (
     <div className={styles.firstContainer}>
-      <h1>First</h1>
+      <div>
+        <div>
+          <Image
+            src="/images/home/hero_sketch.png"
+            width={1405 / 3}
+            height={556 / 3}
+            alt="Technorama 2050 Hero Sketch"
+          />
+        </div>
+        <div className={styles.twentyfifty}>
+          <h1>20</h1>
+          <h1>50</h1>
+        </div>
+      </div>
     </div>
   );
 };
